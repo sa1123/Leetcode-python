db53ef05738274816f3ca50d8102afdc15912840
sa1123 <sahil2398@yahoo.com>
2022-03-22 11:59:41 +0530

    powx-n 2

--- powx-n.py
+++ powx-n.py
@@ -1,11 +1,21 @@
 # https://leetcode.com/problems/powx-n
 
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         if(n == 0):
+#             return 1
+#         if(n < 0):
+#             ans = (1/x)*self.myPow(x, n+1)
+#         else:
+#             ans = x*self.myPow(x, n-1)
+#         return ans
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if(n == 0):
             return 1
-        if(n < 0):
-            ans = (1/x)*self.myPow(x, n+1)
-        else:
-            ans = x*self.myPow(x, n-1)
-        return ans
+        if(n == -1):
+            return 1/x
+        if(n % 2 == 0):
+            return self.myPow(x, n//2) ** 2
+        return (self.myPow(x, n//2) ** 2) * x
